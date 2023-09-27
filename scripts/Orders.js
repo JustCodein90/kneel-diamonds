fa967d9af22445c1a0c029ed5c2a68ada31dd618
Justin Ireland <justinireland90@yahoo.com>
2023-09-26 20:02:39 -0400

    Fix order button to invoke imported function

---
+++ scripts/Orders.js
@@ -0,0 +1,37 @@
+import { getOrders } from "./database.js"
+
+const buildOrderListItem = (order) => {
+    return `<li>
+        Order #${order.id} was placed on ${order.timestamp}
+    </li>`
+}
+
+export const Orders = () => {
+    /*
+        Can you explain why the state variable has to be inside
+        the component function for Orders, but not the others?
+    */
+    const orders = getOrders()
+
+    let html = "<ul>"
+
+    const listItems = orders.map(buildOrderListItem)
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
+
+
+/*
+const metals = getMetals()
+
+// Remember that the function you pass to find() must return true/false
+const foundMetal = metals.find(
+    (metal) => {
+        return metal.id === order.metalId
+    }
+)
+const totalCost = foundMetal.price
+*/
